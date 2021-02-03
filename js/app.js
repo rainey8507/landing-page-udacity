524f96ba7b4834b5ea0e0f0aeb8fce63247a3077
your_rainey8507 <trangle.gg8507@gmail.com>
2021-02-02 21:08:24 -0600

    active class

--- js/app.js
+++ js/app.js
@@ -1,66 +1,47 @@
-/**Define global variable*/
-let menu = document.getElementById("menu");
+// Define Global Variables 
 let navBar = document.querySelector(".nav");
-/* Build Nav */
-    /* Create a new li item dynamically */
-    const navItem1 = document.createElement("li");
-    const navItem2 = document.createElement("li");
-    const navItem3 = document.createElement("li");
-    const navItem4 = document.createElement("li");
-    const navItem5 = document.createElement("li");
-    navItem1.innerHTML = "Home";
-    navItem2.innerHTML = "Service";
-    navItem3.innerHTML = "About";
-    navItem4.innerHTML = "Testimonies";
-    navItem5.innerHTML = "Contact";
-
-    /* Style a navigation list item */
-    navItem1.style.color = "#9cb1b1";
-    navItem2.style.color = "#cfcf8c";
-    navItem3.style.color = "#cfcf8c";
-    navItem4.style.color = "#cfcf8c";
-    navItem5.style.color = "#cfcf8c";
-
-    navItem1.style.listStyle = "none";
-    navItem2.style.listStyle = "none";
-    navItem3.style.listStyle = "none";
-    navItem4.style.listStyle = "none";
-    navItem5.style.listStyle = "none";
-
-    navItem1.style.marginRight = "20px";
-    navItem2.style.marginRight = "20px";
-    navItem3.style.marginRight = "20px";
-    navItem4.style.marginRight = "20px";
-    navItem5.style.marginRight = "20px";
-
-    navItem1.style.fontSize = "20px";
-    navItem2.style.fontSize = "20px";
-    navItem3.style.fontSize = "20px";
-    navItem4.style.fontSize = "20px";
-    navItem5.style.fontSize = "20px";
+const navList = document.querySelector("#menu");
+const NavigationSection = document.querySelectorAll('section'); 
+const links = document.querySelectorAll('.ul li a');
 
 
-    /*Create a tag */
-    let aTag = document.createElement('a');
-    aTag.setAttribute('class', 'menu-link');
+// Build Nav
+function buildNav() {
+    const fragmentNav = document.createDocumentFragment();
+    NavigationSection.forEach((NavigationSection) => {
+        let liTag = document.createElement('li');
+        let aLink = document.createElement('a');
+        liTag.classList.add("nav-link");
+        aLink.innerHTML = NavigationSection.getAttribute("data-navbar");
 
-    aTag.addEventListener("click", () => {
-        document.scrollIntoView({behavior: "smooth"})
+        liTag.appendChild(aLink);
+        navList.appendChild(liTag);
+        fragmentNav.appendChild(liTag);
     });
-    menu.appendChild(navItem1);
-    menu.appendChild(navItem2);
-    menu.appendChild(navItem3);
-    menu.appendChild(navItem4);
-    menu.appendChild(navItem5);
+    navList.appendChild(fragmentNav);
+}
 
+// Active Class 
+function activeState() {
+    let index = NavigationSection.length;
+    while(--index && window.scrollY + 50 < NavigationSection[index].offsetTop) {
+        links.forEach((link) =>link.classList.remove('active'));
+        links[index].classList.add('active');
+    }
+
+}
+
+// Run active state 
+activeState();
+window.addEventListener('scroll', activeState);
 
 /* Make navigation responsive */
-menu.style.maxHeight = "0px";
+navList.style.maxHeight = "0px";
 function togglemenu() {
-    if(menu.style.maxHeight == "0px") {
-        menu.style.maxHeight = "600px";
+    if(navList.style.maxHeight == "0px") {
+        navList.style.maxHeight = "600px";
     } else {
-        menu.style.maxHeight = "0px";
+        navList.style.maxHeight = "0px";
     }
 }
 
@@ -71,3 +52,7 @@ navBar.scrollTo({
     left: 0,
     behavior: 'smooth'
 })
+
+// Build a navigation menu
+buildNav();
+
